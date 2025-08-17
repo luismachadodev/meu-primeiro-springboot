2fadd1a4dcd663f8077c1618ec9f9e049b405675
Luis Henrique <luis@prover.net.br>
2025-08-17 15:19:29 -0300

    concluido curso

---
+++ src/main/java/com/example/meu_primeiro_springboot/HelloController.java
@@ -0,0 +1,18 @@
+package com.example.meu_primeiro_springboot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+    
+    @GetMapping("/hello")
+    public String hello() {
+        return "Olá Mundo! Bem-Vindo ao Spring Boot";
+    }
+    
+}
